671de9b9f23c13bb8cb025bef06c229fb2300678
Nicolas Conte <contenicolascv@gmail.com>
2025-11-23 03:21:40 -0300

    feat(pages): add signin and signup pages

---
+++ src/app/(landing)/components/navigation-items.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/shared/components/ui/button"
+
+
+const items = {
+    "Features": "",
+    "Pricing": "",
+    "Blog": "",
+    "Contact": ""
+}
+export default function NavigationItems() {
+    return (
+        <>
+            {Object.entries(items).map((key, value) =>
+                <Button key={value} className="font-light text-md bg-transparent text-black hover:text-purple-500 hover:bg-transparent cursor-pointer">{key}</Button>
+            )}
+        </>
+    )
+}
