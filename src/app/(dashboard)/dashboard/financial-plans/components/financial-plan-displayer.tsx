d666e13d98e7c6a89b4e950359f26ed1644ae838
Nicolas Conte <contenicolascv@gmail.com>
2025-12-01 02:03:32 -0300

    remove log

--- src/app/(dashboard)/dashboard/financial-plans/components/financial-plan-displayer.tsx
+++ src/app/(dashboard)/dashboard/financial-plans/components/financial-plan-displayer.tsx
@@ -19,7 +19,6 @@ export default function FinancialPlanDisplayer() {
 
     useEffect(() => {
         if (isSuccess && data) {
-            console.log(data);
             setFinancialPlans(data.items);
         }
     }, [isSuccess, data])
