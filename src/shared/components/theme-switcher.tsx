64c2214eb27a1a57a19c2ec654c03c5128fa3458
Nicolas Conte <contenicolascv@gmail.com>
2025-12-01 01:30:19 -0300

    feat: add theme switcher

--- src/shared/components/theme-switcher.tsx
+++ src/shared/components/theme-switcher.tsx
@@ -14,7 +14,7 @@ export default function ThemeSwitcher() {
     }
 
     return (
-        <Button className={`rounded-[50%] absolute top-5 right-10 size-10 text-white cursor-pointer ${theme === 'dark' ? 'bg-orange-500 hover:bg-orange-700' : 'bg-blue-600 hover:bg-blue-800'}`} onClick={changeTheme}>
+        <Button className={`rounded-[50%] absolute bottom-6 right-5 size-10 text-white cursor-pointer ${theme === 'dark' ? 'bg-orange-500 hover:bg-orange-700' : 'bg-blue-600 hover:bg-blue-800'}`} onClick={changeTheme}>
             {theme === 'dark' ? <SunIcon /> : <MoonIcon />}
         </Button>
     )
