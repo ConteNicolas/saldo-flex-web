7b17102bd0ebba3f93702ef8f88afa4bdc4f27d6
Nicolas Conte <contenicolascv@gmail.com>
2025-12-01 01:30:32 -0300

    feat: add function to get relative time

--- src/shared/lib/utils.ts
+++ src/shared/lib/utils.ts
@@ -21,3 +21,35 @@ export function parseObjToQueryString(obj: any) {
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`)
     .join("&");
 }
+
+
+export function getRelativeTime(date: Date): string {
+    if (!date) {
+        return "";
+    }
+
+    const seconds = Math.floor((new Date().getTime() - new Date(date).getTime()) / 1000);
+
+    let interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+        return `${interval} hours`;
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return `${interval} months`;
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return `${interval} days`;
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return `${interval} hours`;
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return `${interval} minutes`;
+    }
+    return `${Math.floor(seconds)} seconds`;
+}
