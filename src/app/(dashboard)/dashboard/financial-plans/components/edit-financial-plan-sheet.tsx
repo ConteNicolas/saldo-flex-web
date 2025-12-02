d9793b4975e6599ea071ec17654a10d56ba231ec
Nicolas Conte <contenicolascv@gmail.com>
2025-12-02 00:47:11 -0300

    feat: add financial plan sheet

---
+++ src/app/(dashboard)/dashboard/financial-plans/components/edit-financial-plan-sheet.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/shared/components/ui/button";
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/shared/components/ui/sheet";
+import { PenIcon, XIcon } from "lucide-react";
+import { EditFinancialPlanForm } from "./edit-financial-plan-form";
+import { IFinancialPlan } from "@/features/financial-plans/models/financial-plan-model";
+
+interface IEditFinancialPlanSheetProps {
+    financialPlan: IFinancialPlan,
+    open: boolean,
+    setOpen: (o: boolean) => void 
+}
+
+export default function EditFinancialPlanSheet({ financialPlan, open, setOpen }: IEditFinancialPlanSheetProps) {
+    return (
+        <Sheet open={open} onOpenChange={setOpen}>
+            <SheetContent>
+                <SheetHeader className="mt-10">
+                    <SheetTitle>Edit financial plan</SheetTitle>
+                    <SheetDescription>
+                        Fill in the form below to update an existing financial plan.
+                    </SheetDescription>
+                </SheetHeader>
+                <EditFinancialPlanForm financialPlan={financialPlan} closeSheet={() => setOpen(false)} />
+                <SheetFooter>
+                    <SheetClose asChild>
+                        <Button variant="outline" className="cursor-pointer">
+                            Close <XIcon />
+                        </Button>
+                    </SheetClose>
+                </SheetFooter>
+            </SheetContent>
+        </Sheet>
+    )
+}
